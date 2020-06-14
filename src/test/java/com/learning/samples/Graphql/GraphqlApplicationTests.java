25bc19ff11e39b5b51290879fe006bc19f0e624e
Ankit Bansal <bansal.ankit@infoedge.com>
2020-06-14 12:45:41 +0530

    added few dependencies

--- src/test/java/com/learning/samples/Graphql/GraphqlApplicationTests.java
+++ src/test/java/com/learning/samples/Graphql/GraphqlApplicationTests.java
@@ -3,7 +3,9 @@ package com.learning.samples.Graphql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.learning.samples.graphql.GraphqlApplication;
+
+@SpringBootTest(classes = GraphqlApplication.class)
 class GraphqlApplicationTests {
 
 	@Test
