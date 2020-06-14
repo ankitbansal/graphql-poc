6e7b600669352f746a7e719f1a0ca19a5de7f878
Ankit Bansal <bansal.ankit@infoedge.com>
2020-06-14 12:01:14 +0530

    Added base project

---
+++ src/test/java/com/learning/samples/Graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.samples.Graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
